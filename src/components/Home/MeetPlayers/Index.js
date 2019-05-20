431592a5c98d4d4a6c81033b9e92e69a5b5d38b6
dorotakora <dorota.kora@o2.pl>
2019-05-20 20:30:01 +0200

    first commit

---
+++ src/components/Home/MeetPlayers/Index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MeetPlayers extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MeetPlayers;
