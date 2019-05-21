c66fda34726cf30833aec8053c65a0a25c047660
dorotakora <dorota.kora@o2.pl>
2019-05-21 08:26:54 +0200

    Updates

---
+++ precache-manifest.7c1e9e651c96ae8b4c380a148132b49c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31804198cbe0351f7154e3c80ccc4b9",
+    "url": "/website_firebase/index.html"
+  },
+  {
+    "revision": "d593e21d454df22d56bb",
+    "url": "/website_firebase/static/css/main.476d91e1.chunk.css"
+  },
+  {
+    "revision": "f750e413ad9b4ae1d316",
+    "url": "/website_firebase/static/js/2.2b1d1478.chunk.js"
+  },
+  {
+    "revision": "d593e21d454df22d56bb",
+    "url": "/website_firebase/static/js/main.fd0b5333.chunk.js"
+  },
+  {
+    "revision": "aae8bf2209a2827aed75",
+    "url": "/website_firebase/static/js/runtime~main.78f931d6.js"
+  },
+  {
+    "revision": "5432bcd5600bfba5ec63967d7efb05b9",
+    "url": "/website_firebase/static/media/Otamendi.5432bcd5.png"
+  },
+  {
+    "revision": "6de3706ade22bf1989b3853dda736986",
+    "url": "/website_firebase/static/media/featured_player.6de3706a.png"
+  },
+  {
+    "revision": "1d5b64ec5f8fe8089fd9370edd244ac4",
+    "url": "/website_firebase/static/media/manchester_city_logo.1d5b64ec.png"
+  }
+]);
